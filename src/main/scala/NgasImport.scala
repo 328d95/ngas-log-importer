c0e9f38337ed69dd7b11224855162df19dad2c69
Damien Frew <20500861@student.uwa.edu.au>
2015-08-11 00:54:14 -0700

    Add basic elasticsearch saving.

--- src/main/scala/NgasImport.scala
+++ src/main/scala/NgasImport.scala
@@ -3,6 +3,8 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
+import org.elasticsearch.spark._
+
 object NgasImport {
     
     val ngasAccessRegex = """^(\d{4}\-\d{2}\-\d{2})T(\d{2}\:\d{2}:\d{2}\.\d{3})\s.*(?<=client_address=\(\')(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\'\,\s+(\d+).*(?<=file\_id=)(\d+)\_(\d+).*(?<=host=)([^\s]+).*(?<=user\-agent=)([^\s]+)""".r
@@ -13,19 +15,24 @@ object NgasImport {
           .setMaster("local[4]")
           .setAppName("NGAS Log Importer")
           .set("spark.executor.memory", "2g")
+          .set("es.index.auto.create", "true")
         val sc = new SparkContext(conf)
-        val accesses = sc.textFile(logFile).cache()
+        val accesses = sc.textFile(logFile)
           .filter(line => line.contains("Handling HTTP request:"))
           .filter(line => line.contains("method=GET"))
           .map(line => extractValues(line))
+        accesses.saveToEs("spark/test")
         sc.stop()
     }
 
-    def extractValues(line: String): (String, String, String, String, String, String, String, String) = {
+    def extractValues(line: String): Map[String, String] = {
       ngasAccessRegex.findFirstIn(line) match {
         case Some(ngasAccessRegex(date, time, ip, port, obsId, obsDate, host, agent)) =>
-          (date, time, ip, port, obsId, obsDate, host, agent)
-        case _ => (null, null, null, null, null, null, null, null)
+          Map( "date" -> date, "time" -> time, "ip" -> ip, "port" -> port, 
+            "obsId" -> obsId, "obsDate" -> obsDate, "host" -> host, "agent" -> agent)
+        case _ => 
+          Map( "date" -> null, "time" -> null, "ip" -> null, "port" -> null, 
+            "obsId" -> null, "obsDate" -> null, "host" -> null, "agent" -> null)
       }
     }
 }
